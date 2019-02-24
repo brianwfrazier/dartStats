6aa28bc639c98e08fa6f80dac99bd6cf093a7ba7
Brian Frazier <brianwfrazier@Brians-MacBook-Air-2.local>
2019-02-24 14:26:20 -0500

    Static version completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React , { Component } from 'react';
 import './App.css';
+import Header from './components/header' 
+import ContactForm from './components/contactForm' 
+import ContactList from './components/contactList' ;
+let contacts = [
+  {
+      "name": "Contact 1",
+      "address": "123 Test St",
+      "phone_number": "132-3212"
+  },       
+  {
+      "name": "Contact 2",
+      "address": "23 Main St",
+      "phone_number": "934-4329"
+  },        
+  {
+      "name": "Contact 3",
+      "address": "4 Lower St",
+      "phone_number": "432-5832"
+  },            
+  {
+      "name": "Contact 4",
+      "address": "49 Upper Street",
+      "phone_number": "934-4290"
+  },
+  {
+      "name": "Contact 5",
+      "address": "4 High Street",
+      "phone_number": "933-4110"
+  }                
+] ;    
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="jumbotron">
+          <Header />
+          <ContactForm  />
+          <ContactList contacts={contacts} />
       </div>
     );
   }
