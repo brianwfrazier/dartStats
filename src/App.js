0e2318c45c306b8dd5fd823d230a749961d27de7
Brian Frazier <brianwfrazier@Brians-MacBook-Air-2.local>
2019-03-10 13:36:02 -0400

    added stub API

--- src/App.js
+++ src/App.js
@@ -2,45 +2,20 @@ import React , { Component } from 'react';
 import './App.css';
 import Header from './components/header' 
 import ContactForm from './components/contactForm' 
-import ContactList from './components/contactList' ;
-let contacts = [
-  {
-      "name": "Contact 1",
-      "address": "123 Test St",
-      "phone_number": "132-3212"
-  },       
-  {
-      "name": "Contact 2",
-      "address": "23 Main St",
-      "phone_number": "934-4329"
-  },        
-  {
-      "name": "Contact 3",
-      "address": "4 Lower St",
-      "phone_number": "432-5832"
-  },            
-  {
-      "name": "Contact 4",
-      "address": "49 Upper Street",
-      "phone_number": "934-4290"
-  },
-  {
-      "name": "Contact 5",
-      "address": "4 High Street",
-      "phone_number": "933-4110"
-  }                
-] ;    
+import ContactList from './components/contactList' 
+import api from './dataStore/stubAPI'  // NEW
 
 class App extends Component {
-  render() {
-    return (
-      <div className="jumbotron">
-          <Header />
-          <ContactForm  />
-          <ContactList contacts={contacts} />
-      </div>
-    );
-  }
+    render() {
+        let contacts = api.getAll() ;    // NEW
+        return (    
+            <div className="jumbotron">
+                <Header noContacts={contacts.length} />
+                <ContactForm />
+                <ContactList contacts={contacts}  />  
+            </div>                      
+            );
+    }
 }
 
 export default App;
